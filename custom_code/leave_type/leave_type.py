d2c05f69ef684643005a77fa2a93de2aae0d344a
Mukesh <jaissunny009@gmail.com>
2024-10-24 12:05:36 +0545

    Home and Sick Leave

--- custom_code/leave_type/leave_type.py
+++ custom_code/leave_type/leave_type.py
@@ -1,38 +1,53 @@
 import frappe 
 
-def create_leave_type():
-    
-    #Home Leave 
+def create_leave_type(leave_type_name, max_leaves_allowed, applicable_after, max_continuous_days_allowed=None, is_earned_leave=1, earned_leave_frequency="Monthly", allocate_on_day="First Day", rounding="0.5", is_carry_forward=0, include_holiday=1, allow_encashment=0, maximum_carry_forwarded_leaves=None, max_encashable_leaves=None, earning_component=None):
     doc = frappe.new_doc("Leave Type")
-    doc.leave_type_name = "Annual/Home Leave"
-    doc.max_leave_allowed = "18"
-    doc.applicable_after = "20"
-    doc.max_continuous_days_allowed = ""
-    doc.include_holiday = 1
-    
-    doc.is_earned_leave = 1
-    doc.earned_leave_frequency = "Monthly"
-    doc.allocate_on_day = "First Day"
-    doc.rounding = "0.5"
+    doc.leave_type_name = leave_type_name
+    doc.max_leaves_allowed = max_leaves_allowed
+    doc.applicable_after = applicable_after
+    doc.max_continuous_days_allowed = max_continuous_days_allowed
+    doc.is_earned_leave = is_earned_leave
+    doc.earned_leave_frequency = earned_leave_frequency
+    doc.allocate_on_day = allocate_on_day
+    doc.rounding = rounding
+    doc.include_holiday = include_holiday
+    doc.is_carry_forward = is_carry_forward
+    if is_carry_forward:
+        doc.maximum_carry_forwarded_leaves = maximum_carry_forwarded_leaves
+    if allow_encashment:
+        doc.allow_encashment = allow_encashment
+        doc.max_encashable_leaves = max_encashable_leaves
+    if earning_component:
+        doc.earning_component = earning_component
     doc.save()
 
-    #Sick Leave
-    doc = frappe.new_doc("Leave Type")
-    doc.leave_type_name = "Sick Leave Compliance"
-    doc.max_leaves_allowed = "12"
-    doc.applicable_after = "20"
-    doc.max_continuous_days_allowed = "3"
-    doc.is_carry_forward = 1
-    doc.include_holiday = 1
-    doc.maximum_carry_forwarded_leaves = "45"
+    leave_types = [
+        {
+            #Home Leave
+            "leave_type_name": "Annual/Home Leave",
+            "max_leaves_allowed": "18",
+            "applicable_after": "20",
+            # "allocate_on_day": "First Day",
+        },
+        {
+            #Sick Leave 
+            "leave_type_name": "Sick Leaves",
+            "max_leaves_allowed": "12",
+            "applicable_after": "20",
+            "max_continuous_days_allowed": "3",
+            "is_carry_forward": 1,
+            "maximum_carry_forwarded_leaves": "45",
+            "allow_encashment": 1,
+            "max_encashable_leaves": "45",
+            "earning_component": "Basic",
+            "allocate_on_day": "Last Day",
+        }
+    ]
 
-    doc.allow_encashment = 1
-    doc.max_encashable_leaves = "45"
-    doc.earning_component = "Basic"
-    doc.is_earned_leave = 1
-    doc.earned_leave_frequency = "Monthly"
-    doc.allocate_on_day = "Last Day"
-    doc.rounding = "0.5"
-    doc.save()
+    for leave_type in leave_types:
+        create_leave_type(**leave_type)
+
+
+
 
 
