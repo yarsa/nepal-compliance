deb4487c47ce81dad5a4b2e4a3778417ea67dac5
Mukesh <jaissunny009@gmail.com>
2024-11-06 16:26:44 +0545

    Purchase Vat Register Report Filter

--- nepal_compliance/report/purchase_vat_register/purchase_vat_register.js
+++ nepal_compliance/report/purchase_vat_register/purchase_vat_register.js
@@ -2,5 +2,40 @@
 // For license information, please see license.txt
 
 frappe.query_reports["Purchase Vat Register"] = {
-    
+    "filters": [
+        {
+            fieldname: 'company',
+            label: __('Comoany'),
+            fieldtype: 'Link',
+            options: 'Company',
+            default: frappe.default.get_user_default('company')
+        },
+        {
+            fieldname: 'supplier',
+            label: __('Supplier'),
+            fieldtype: 'Link',
+            options: 'Supplier'
+        },
+        {
+            fieldname: 'bill_no',
+            label: __('Supplier Invoice No'),
+            fieldtype: 'Data',
+            reqd: 0
+        },
+        {
+            fieldname: 'bill_date',
+            label: __('Supplier Invoice Date'),
+            fieldtype: 'Date',
+        },
+        {
+            fieldname: 'due_date',
+            label: __('Invoice Due Date'),
+            fieldtype: 'Data'
+        },
+        {
+            fieldname: 'is_vat_applicable',
+            label: __('Is Vat Applicable'),
+            fieldtype: 'Check'
+        }
+    ]
 };
