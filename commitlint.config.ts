c4ef2445fbb1589dcc4b86b345a4114d1a16169b
Mukesh <jaissunny009@gmail.com>
2024-11-19 11:21:25 +0545

    chore(commitlint and husky): conventional commit pattern configuration

---
+++ commitlint.config.ts
@@ -0,0 +1,49 @@
+import type {UserConfig} from '@commitlint/types'; 
+
+const Configuration: UserConfig = {
+
+  extends: ['@commitlint/config-conventional'],
+
+  rules: {
+
+    // 'type-enum': [RuleConfigSeverity.Error, 'always', ['foo']],
+
+    'type-enum' : [
+
+        2,
+
+        'always',
+
+        [
+
+            'feat',   //New features
+
+            'fix',    // Bug fixes
+
+            'chore',  // Maintenance tasks
+
+            'ci',     //CI configuration changes
+
+            'docs',    //Documentation updates
+
+            'perf',   // Performance Improvements
+
+            'refactor',   // Code Refactoring
+
+            'revert',   //Revert to previous commit
+
+            'style',   // Code style changes
+
+            'test',   // Adding or Updating tests
+            
+            //other rules as requred
+
+        ],
+
+    ] ,
+
+  },
+
+};
+
+module.exports = Configuration;
