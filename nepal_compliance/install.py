ec1c601203bab6d8e697e0b9346f9c77a1f8bbae
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2025-01-15 08:20:35 +0000

    refactor(directory structure): directory structure refactored for app installation

---
+++ nepal_compliance/install.py
@@ -0,0 +1,10 @@
+import frappe
+
+from nepal_compliance.custom_code.payroll.salary_component import create_salary_component
+from nepal_compliance.custom_code.payroll.income_tax_slab import create_income_tax_slab
+from nepal_compliance.custom_code.leave_type.leave_type import create_leave_type
+
+def install():
+    create_salary_component()
+    create_income_tax_slab()
+    create_leave_type(**leave_type)
