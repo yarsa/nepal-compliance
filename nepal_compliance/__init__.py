aa4df80b092efb597c418e782df23650bc91b5c0
S-mkc <jaissunny009@gmail.com>
2025-01-24 10:12:15 +0000

    chore(pyproject): pyproject.toml setup coofiguration added

---
+++ nepal_compliance/__init__.py
@@ -0,0 +1,4 @@
+import frappe
+
+__version__ = "1.0.0-dev"
+__module_name__ = 'nepal_compliance'
--- setup.py
+++ setup.py
@@ -1,16 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
+name = "nepal_compliance"
 
-setup(
-    name="nepal_compliance",
-    version="0.0.1",
-    description="ERPNext app to comply with Nepali laws and regulations",
-    author="Yarsa Labs Pvt. Ltd.",
-    author_email="support@yarsalabs.com",
-    packages=find_packages(),
-    zip_safe=False,
-    include_package_data=True,
-    install_requires=install_requires
-)
+setup()
