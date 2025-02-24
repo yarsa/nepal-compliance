04233d57ddcedf0dc83c51406597e9ac17982657
S-mkc <jaissunny009@gmail.com>
2025-02-24 07:04:13 +0000

    chore(party_wise_purchase/sales_register, custom_field): resolve multiple assignments of outstanding, remove duplicate shift assignment for custom field, and update fieldtype to fieldname in purchase_vat_register.py

--- nepal_compliance/nepal_compliance/report/party_wise_purchase_register/party_wise_purchase_register.py
+++ nepal_compliance/nepal_compliance/report/party_wise_purchase_register/party_wise_purchase_register.py
@@ -75,7 +75,6 @@ def execute(filters=None):
         "qty": 0,
         "rate": 0,  
         "amount": 0,
-        "outstanding": 0,
         "taxes_and_charges_added": 0,
         "discount_amount": 0,
         "total": 0,
@@ -117,7 +116,6 @@ def execute(filters=None):
             invoice_totals[current_invoice] = {
                 "qty": 0,
                 "amount": 0,
-                "outstanding": 0,
                 "rate": 0, 
                 "taxes_and_charges_added": 0,
                 "discount_amount": 0,
