ec539caafbb77500d2e17d8a64659cf5d89365a9
S-mkc <jaissunny009@gmail.com>
2025-04-21 11:10:39 +0000

    modify changes to license text and utils code convention

--- nepal_compliance/nepal_compliance/doctype/nepal_compliance_settings/test_nepal_compliance_settings.py
+++ nepal_compliance/nepal_compliance/doctype/nepal_compliance_settings/test_nepal_compliance_settings.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2025, Yarsa Labs Pvt. Ltd. and Contributors
-# See license.txt
+# For license information, please see LICENSE at the root of this repository
 
 # import frappe
 from frappe.tests.utils import FrappeTestCase
