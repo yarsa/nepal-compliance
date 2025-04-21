fb3cd4c73187e3670bc378a74732f64809a86a60
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2025-04-21 17:00:24 +0545

    back-dated doctypes submission, setup salary structure and supplier/customer read-only (#49)
    * feat(backdated-doctype-restriction): restrict backdated submission; also add salary structure setup and required fields in supplier/customer masters
    
    * License text edited
    
    * modify changes to license text and utils code convention

--- nepal_compliance/public/js/utils.js
+++ nepal_compliance/public/js/utils.js
@@ -1,7 +1,7 @@
 frappe.ui.form.on('Sales Invoice', {
     refresh: function(frm) {
-            frm.set_df_property('cbms_status', 'read_only', 1);
-            frm.set_df_property('cbms_response', 'read_only', 1);
+            frm.set_df_property('cbms_status', 'read_only', 1),
+            frm.set_df_property('cbms_response', 'read_only', 1),
             frm.set_df_property('vat_number', 'read_only', 1),
             frm.set_df_property('supplier_vat_number', 'read_only', 1)
         }
