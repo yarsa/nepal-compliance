14a75a906b17f75cf118375f9c3f1cf9c54e2451
Mukesh Kr. Chaudhary <97079101+S-mkc@users.noreply.github.com>
2025-04-22 12:15:07 +0545

    Add feature of restricting back-dated doctype submission (#50)
    * back-dated doctypes submission, setup salary structure and supplier/customer read-only (#49)
    
    * feat(backdated-doctype-restriction): restrict backdated submission; also add salary structure setup and required fields in supplier/customer masters
    
    * License text edited
    
    * modify changes to license text and utils code convention

---
+++ nepal_compliance/nepal_compliance/doctype/nepal_compliance_role/nepal_compliance_role.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Yarsa Labs Pvt. Ltd. and contributors
+# For license information, please see LICENSE at the root of this repository
+
+# import frappe
+from frappe.model.document import Document
+
+
+class NepalComplianceRole(Document):
+	pass
