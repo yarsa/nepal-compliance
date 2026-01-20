8ad7b054c95bf4d3b90048ab5eaf7ac86f6ecfa0
S-mkc <jaissunny009@gmail.com>
2026-01-20 05:44:04 +0000

    refactor(custom_bs_fields): refactor js custom nepali date capturing with python utility along with backward compatibility

--- nepal_compliance/hooks.py
+++ nepal_compliance/hooks.py
@@ -59,40 +59,11 @@ boot_session = "nepal_compliance.boot.get_boot_info"
 # include js in doctype views
 doctype_js = {
     "Company": "public/js/validate.js",
-    "User": "public/js/nepali_date.js",
-    "Salary Slip" : "public/js/salary_slip.js",
-    "Expense Claim": "public/js/bs_date.js",
-    "Leave Application": "public/js/bs_date.js",
-    "Holiday List": ["public/js/bs_date.js","public/js/holiday_list.js"],
-    "Holiday": "public/js/holiday_list.js",
-    "Leave Allocation": "public/js/bs_date.js",
-    "Attendance": "public/js/bs_date.js",
-    "Fiscal Year": "public/js/bs_date.js",
-    "Stock Entry": "public/js/hrms_bs_date.js",
-    "Material Request": "public/js/hrms_bs_date.js",
-    "Purchase Invoice": ["public/js/bs_date.js", "public/js/utils.js", "public/js/validate.js", "public/js/email.js"],
-    "Purchase Order": "public/js/bs_date.js","Purchase Receipt": "public/js/bs_date.js",
-    "Sales Order": "public/js/bs_date.js","Delivery Note": "public/js/bs_date.js",
-    "Sales Invoice": ["public/js/bs_date.js", "public/js/utils.js", "public/js/validate.js", "public/js/email.js"],
+    "Purchase Invoice": ["public/js/utils.js", "public/js/validate.js", "public/js/email.js"],
+    "Sales Invoice": ["public/js/utils.js", "public/js/validate.js", "public/js/email.js"],
     "CBMS Settings": "nepal_compliance/doctype/cbms_settings/cbms_settings.js",
-    "Payment Entry": "public/js/bs_date.js",
-    "Journal Entry": ["public/js/bs_date.js", "public/js/utils.js"],
     "Supplier": "public/js/validate.js",
     "Customer": "public/js/validate.js",
-    "Request for Quotation": "public/js/bs_date.js","Supplier Quotation": "public/js/bs_date.js", "Quotation": "public/js/bs_date.js",
-    "Blanket Order": "public/js/bs_date.js",
-    "Landed Cost Voucher": "public/js/bs_date.js",
-    "Asset": "public/js/bs_date.js", "Asset Repair": "public/js/bs_date.js", "Asset Movement": "public/js/bs_date.js", "Asset Value Adjustment": "public/js/bs_date.js", "Asset Capitalization": "public/js/bs_date.js",
-    "POS Opening Entry": "public/js/bs_date.js", "POS Closing Entry": "public/js/bs_date.js",
-    "Loyalty Program": "public/js/bs_date.js", "Promotional Scheme": "public/js/bs_date.js", "Pricing Rule": "public/js/bs_date.js", "Coupon Code": "public/js/bs_date.js",
-    "Serial No": "public/js/bs_date.js", "Batch": "public/js/bs_date.js",
-    "Installation Note": "public/js/bs_date.js", "Stock Reconciliation": "public/js/bs_date.js", "Quality Inspection": "public/js/bs_date.js", "Quick Stock Balance": "public/js/bs_date.js",
-    "Payroll Entry": "public/js/payroll_bs_date.js", "Income Tax Slab": "public/js/payroll_bs_date.js", "Payroll Period": "public/js/payroll_bs_date.js", "Salary Structure Assignment": ["public/js/payroll_bs_date.js"], "Salary Withholding": "public/js/payroll_bs_date.js", "Additional Salary": "public/js/payroll_bs_date.js", "Employee Incentive": "public/js/payroll_bs_date.js", "Retention Bonus": "public/js/payroll_bs_date.js",
-    "Employee Tax Exemption Proof Submission": "public/js/payroll_bs_date.js", "Employee Benefit Application": "public/js/payroll_bs_date.js", "Employee Benefit Claim": "public/js/hrms_bs_date.js",
-    "Attendance Request": "public/js/hrms_bs_date.js", "Compensatory Leave Request": "public/js/hrms_bs_date.js", "Employee Advance": "public/js/hrms_bs_date.js", "Shift Assignment": "public/js/hrms_bs_date.js", "Shift Request": "public/js/hrms_bs_date.js", "Job Offer": "public/js/hrms_bs_date.js", "Employee Referral": "public/js/hrms_bs_date.js", "Shift Assignment Tool": "public/js/hrms_bs_date.js",
-    "Upload Attendance": "public/js/hrms_bs_date.js", "Leave Period": "public/js/hrms_bs_date.js", "Leave Policy Assignment": "public/js/hrms_bs_date.js", "Leave Control Panel": "public/js/hrms_bs_date.js", "Leave Encashment": "public/js/hrms_bs_date.js",
-    "Bulk Salary Structure Assignment": "public/js/bs_date.js", "Employee Attendance Tool": 'public/js/bs_date.js',
-    "Period Closing Voucher": "public/js/bs_date.js", "Invoice Discounting": "public/js/bs_date.js", "Dunning": "public/js/bs_date.js", "Process Deferred Accounting": "public/js/bs_date.js", "POS Invoice": "public/js/bs_date.js"
 }
 
 doctype_list_js = {
@@ -129,10 +100,12 @@ doctype_list_js = {
 # ----------
 
 # add methods and filters to jinja environment
-# jinja = {
-# 	"methods": "nepal_compliance.utils.jinja_methods",
-# 	"filters": "nepal_compliance.utils.jinja_filters"
-# }
+jinja = {
+    "methods": ["nepal_compliance.nepali_date_utils.nepali_date.format_bs",
+                "nepal_compliance.nepali_date_utils.nepali_date.format_bs_datetime",
+                "nepal_compliance.nepali_date_utils.utils.bs_date"
+                ]
+}
 
 # Installation
 # ------------
